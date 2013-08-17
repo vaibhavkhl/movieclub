2c0de4d52d05d24942485b00eeed12279b678d12
unknown <vaibhav.khl@gmail.com>
2013-08-18 00:52:30 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Movieclub::Application.config.secret_key_base = '4041363d7bcf14c62754006bcceb0c3a7a5c653851145e2644f60762e2bda6be60484171e2f73d10f26a1a82b5ac305e9b906cd905867cf15d7a4c8310304687'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Movieclub::Application.config.session_store :cookie_store, key: '_movieclub_session'
