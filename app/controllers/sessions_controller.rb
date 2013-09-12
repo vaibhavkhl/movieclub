82abef5f65132dd9c78165964563c136f30a79e4
unknown <vaibhav.khl@gmail.com>
2013-09-13 02:23:32 +0530

    view users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ def new
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     	sign_in user
-      redirect_to user
+      redirect_back_or user
       # Sign the user in and redirect to the user's show page.
     else
       flash.now[:error] = 'Invalid email/password combination' 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:index, :edit, :update]
+	 before_action :correct_user,   only: [:edit, :update]
+  
+  def index
+    @users = User.all
+  end
+  
   def show
   	@user = User.find(params[:id])
   end
+  
   def new
   	@user = User.new
   end
-   def create
+  
+  def create
     @user = User.new(user_params)
     if @user.save
     	sign_in @user
@@ -15,6 +24,22 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+    
+  def edit
+    	@user = User.find(params[:id])
+  end
+    
+  def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "Profile Updated"
+        sign_in @user
+        redirect_to @user
+      else
+        render 'edit'
+      end
+  end
+ 
 
   private
 
@@ -22,4 +47,33 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+    
+    def signed_in_user
+      unless signed_in?
+        store location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
+    end
+
+    def edit
+    	@user = User.find(params[:id])
+    end
+    
+    def update
+       @user = User.find(params[:id])
+       if @user.update_attributes(user_params)
+          flash[:success] = "Profile updated"
+          sign_in @user
+          redirect_to @user
+       else	
+       	render 'edit'
+       	end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
+
